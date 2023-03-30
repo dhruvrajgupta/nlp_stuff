e8458397c3f4cde5b99a9103b1a23befe958db99
dhruv.raj <dhruv.gupta@the-chainless.com>
2023-03-30 16:58:19 +0200

    combining

---
+++ project/trf.py
@@ -0,0 +1,19 @@
+import spacy
+from thinc.api import set_gpu_allocator, require_gpu
+
+# Use the GPU, with memory allocations directed via PyTorch.
+# This prevents out-of-memory errors that would otherwise occur from competing
+# memory pools.
+# set_gpu_allocator("pytorch")
+# require_gpu(0)
+
+nlp = spacy.load("en_core_web_trf")
+# for doc in nlp.pipe(["some text", "some other text"]):
+#     print(doc)
+#     tokvecs = doc._.trf_data.tensors[-1]
+
+text = nlp.pipe(["Australian tennis player"])
+for doc in text:
+    print(doc)
+    tokvecs = doc._.trf_data.tensors[-1]
+    print(tokvecs.shape)
