80e879482887a5df609b55da4558da74b08ca085
dhruv.raj <dhruv.gupta@the-chainless.com>
2023-03-30 19:03:26 +0200

    refactoring

--- project/el_training.py
+++ project/el_training.py
@@ -2,18 +2,43 @@ import os
 import spacy
 from spacy.kb import InMemoryLookupKB
 import json
-from collections import Counter
 import random
-from spacy.util import minibatch, compounding
-from spacy.training import Example
-from spacy.ml.models import load_kb
 import pickle
-
 
 import csv
 from pathlib import Path
 
-output_dir = Path.cwd() / "my_outputx"
+output_dir = Path.cwd() / "my_output"
+dis_kb = {}
+nlp = spacy.load("en_core_web_trf")
+
+
+def get_embeddings_from_string(str: String):
+    print(str)
+    document = nlp("This is a sentence. This is another sentence.")
+    sentences = [sentence.text for sentence in document.sents]
+    desc_doc = nlp.pipe(sentences)
+
+    desc_embeddings_stack = []
+    desc_avg_enc = None
+    for sentence in desc_doc:
+        # print(doc)
+        desc_enc = doc._.trf_data.tensors[-1]  # (1, 768)
+        desc_embeddings_stack.append(desc_enc)
+        desc_avg_enc = get_avg_vectors(desc_embeddings_stack)
+    # print(vector_stack.shape)
+    # vector_stack = []
+    # vector_stack.append(np.array([[1,2]]))
+    # vector_stack.append(np.array([[7,5]]))
+    sum = 0
+    count = 0
+    for embedding in vector_stack:
+        # print(embedding[0])
+        sum += embedding[0]
+        count += 1
+
+    return sum / count
+
 
 def load_entities():
     entities_loc = Path.cwd() / "input" / "entities.csv"  # distributed alongside this notebook
@@ -28,38 +53,53 @@ def load_entities():
             desc = row[2]
             names[qid] = name
             descriptions[qid] = desc
+            dis_kb[qid] = {"name": name, "desc": desc}
     return names, descriptions
+
 
 def create_kb():
     nlp = spacy.load("en_core_web_trf")
-    text = "Tennis champion Emerson was expected to win Wimbledon."
-    doc = nlp(text)
+    # text = "Tennis champion Emerson was expected to win Wimbledon."
+    # doc = nlp(text)
     name_dict, desc_dict = load_entities()
 
     # for QID in name_dict.keys():
     #     print(f"{QID}, name={name_dict[QID]}, desc={desc_dict[QID]}")
 
-    kb = InMemoryLookupKB(vocab=nlp.vocab, entity_vector_length=64)
+    kb = InMemoryLookupKB(vocab=nlp.vocab, entity_vector_length=768)
     for qid, desc in desc_dict.items():
-        print(desc)
-        desc_doc = nlp(desc)
-        print(type(desc_doc))
-        desc_enc = desc_doc.vector
-        print(desc_enc)
-        kb.add_entity(entity=qid, entity_vector=desc_enc, freq=342)   # 342 is an arbitrary value here
+        # Desc is very short,  long -> use sentencizer and average the tokens
+        doc = nlp(desc)
+        sentences = [sent.text for sent in doc.sents]
+        desc_doc = nlp.pipe(sentences)
+        desc_embeddings_stack = []
+        desc_avg_enc = None
+        for doc in desc_doc:
+            # print(doc)
+            desc_enc = doc._.trf_data.tensors[-1]  # (1, 768)
+            desc_embeddings_stack.append(desc_enc)
+            desc_avg_enc = get_avg_vectors(desc_embeddings_stack)
+
+        kb.add_entity(entity=qid, entity_vector=desc_avg_enc, freq=342)  # 342 is an arbitrary value here
 
     for qid, name in name_dict.items():
-        kb.add_alias(alias=name, entities=[qid], probabilities=[1])   # 100% prior probability P(entity|alias)
+        # print(qid)
+        # print(name)
+        kb.add_alias(alias=name, entities=[qid], probabilities=[1])  # 100% prior probability P(entity|alias)
 
     qids = name_dict.keys()
     probs = [0.3 for qid in qids]
     kb.add_alias(alias="Emerson", entities=qids, probabilities=probs)  # sum([probs]) should be <= 1 !
+
+    print(f"Entities in the KB: {kb.get_entity_strings()}")
+    print(f"Aliases in the KB: {kb.get_alias_strings()}")
 
     # change the directory and file names to whatever you like
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     kb.to_disk(output_dir / "my_kb")
     nlp.to_disk(output_dir / "my_nlp")
+
 
 def train_el():
     nlp = spacy.load(output_dir / "my_nlp")
@@ -104,46 +144,49 @@ def train_el():
     random.shuffle(train_dataset)
     random.shuffle(test_dataset)
 
-    TRAIN_EXAMPLES = []
-    if "sentencizer" not in nlp.pipe_names:
-        nlp.add_pipe("sentencizer")
-    sentencizer = nlp.get_pipe("sentencizer")
-    for text, annotation in train_dataset:
-        example = Example.from_dict(nlp.make_doc(text), annotation)
-        # Here annotation is the Gold Standard reference
-        example.reference = sentencizer(example.reference)
-        TRAIN_EXAMPLES.append(example)
-
-    entity_linker = nlp.add_pipe("entity_linker", config={"incl_prior": False}, last=True)
-    entity_linker.initialize(get_examples=lambda: TRAIN_EXAMPLES, kb_loader=load_kb(output_dir / "my_kb"))
-
-    with nlp.select_pipes(enable=["entity_linker"]):   # train only the entity_linker
-        optimizer = nlp.resume_training()
-        for itn in range(500):   # 500 iterations takes about a minute to train
-            random.shuffle(TRAIN_EXAMPLES)
-            batches = minibatch(TRAIN_EXAMPLES, size=compounding(4.0, 32.0, 1.001))  # increasing batch sizes
-            losses = {}
-            for batch in batches:
-                nlp.update(
-                    batch,   
-                    drop=0.2,      # prevent overfitting
-                    losses=losses,
-                    sgd=optimizer,
-                )
-            if itn % 50 == 0:
-                print(itn, "Losses", losses)   # print the training loss
-    print(itn, "Losses", losses)
-
-    nlp.to_disk(output_dir / "my_nlp_el")
+    # TRAIN_EXAMPLES = []
+    # if "sentencizer" not in nlp.pipe_names:
+    #     nlp.add_pipe("sentencizer")
+    # sentencizer = nlp.get_pipe("sentencizer")
+    # for text, annotation in train_dataset:
+    #     example = Example.from_dict(nlp.make_doc(text), annotation)
+    #     # Here annotation is the Gold Standard reference
+    #     example.reference = sentencizer(example.reference)
+    #     TRAIN_EXAMPLES.append(example)
+    #
+    # entity_linker = nlp.add_pipe("entity_linker", config={"incl_prior": False}, last=True)
+    # entity_linker.initialize(get_examples=lambda: TRAIN_EXAMPLES, kb_loader=load_kb(output_dir / "my_kb"))
+    #
+    # with nlp.select_pipes(enable=["entity_linker"]):  # train only the entity_linker
+    #     optimizer = nlp.resume_training()
+    #     for itn in range(500):  # 500 iterations takes about a minute to train
+    #         random.shuffle(TRAIN_EXAMPLES)
+    #         batches = minibatch(TRAIN_EXAMPLES, size=compounding(4.0, 32.0, 1.001))  # increasing batch sizes
+    #         losses = {}
+    #         for batch in batches:
+    #             nlp.update(
+    #                 batch,
+    #                 drop=0.2,  # prevent overfitting
+    #                 losses=losses,
+    #                 sgd=optimizer,
+    #             )
+    #         if itn % 50 == 0:
+    #             print(itn, "Losses", losses)  # print the training loss
+    # print(itn, "Losses", losses)
+    #
+    # nlp.to_disk(output_dir / "my_nlp_el")
 
     with open(output_dir / "test_set.pkl", "wb") as f:
         pickle.dump(test_dataset, f)
 
 
 if __name__ == "__main__":
+    # get_avg_vectors()
     create_kb()
     train_el()
-    nlp = spacy.load(output_dir / "my_nlp_el")
+    nlp = spacy.load(output_dir / "my_nlp")
+    kb = InMemoryLookupKB(vocab=nlp.vocab, entity_vector_length=0)
+    kb.from_disk(output_dir / "my_kb")
     # text = "Tennis champion Emerson was expected to win Wimbledon."
     # doc = nlp(text)
     # for ent in doc.ents:
@@ -152,11 +195,18 @@ if __name__ == "__main__":
     with open(output_dir / "test_set.pkl", "rb") as f:
         test_dataset = pickle.load(f)
 
+    print(dis_kb)
     for text, true_annot in test_dataset:
         print(text)
         print(f"Gold annotation: {true_annot}")
         doc = nlp(text)  # to make this more efficient, you can use nlp.pipe() just once for all the texts
-        for ent in doc.ents:
-            if ent.text == "Emerson":
-                print(f"Prediction: {ent.text}, {ent.label_}, {ent.kb_id_}")
-        print()
+        text_vector = doc.vector
+        print(text_vector)
+        # for ent in doc.ents:
+        #     if ent.text == "Emerson":
+        #         # Get all candidates for Emerson
+        #         candidates = kb.get_alias_candidates("Emerson")
+        #         for candidate in candidates:
+        #             print(f"{candidate.entity_} - {dis_kb[candidate.entity_]}")
+        #         # print(f"Prediction: {ent.text}, {ent.label_}, {ent.kb_id_}")
+        # print()
--- project/trf.py
+++ project/trf.py
@@ -1,19 +1,29 @@
-import spacy
-from thinc.api import set_gpu_allocator, require_gpu
-
-# Use the GPU, with memory allocations directed via PyTorch.
-# This prevents out-of-memory errors that would otherwise occur from competing
-# memory pools.
-# set_gpu_allocator("pytorch")
-# require_gpu(0)
-
-nlp = spacy.load("en_core_web_trf")
-# for doc in nlp.pipe(["some text", "some other text"]):
+# import spacy
+# from thinc.api import set_gpu_allocator, require_gpu
+#
+# # Use the GPU, with memory allocations directed via PyTorch.
+# # This prevents out-of-memory errors that would otherwise occur from competing
+# # memory pools.
+# # set_gpu_allocator("pytorch")
+# # require_gpu(0)
+#
+# nlp = spacy.load("en_core_web_trf")
+# # for doc in nlp.pipe(["some text", "some other text"]):
+# #     print(doc)
+# #     tokvecs = doc._.trf_data.tensors[-1]
+#
+# text = nlp.pipe(["Australian tennis player"])
+# for doc in text:
 #     print(doc)
 #     tokvecs = doc._.trf_data.tensors[-1]
+#     print(tokvecs.shape)
 
-text = nlp.pipe(["Australian tennis player"])
-for doc in text:
-    print(doc)
-    tokvecs = doc._.trf_data.tensors[-1]
-    print(tokvecs.shape)
+
+# from spacy.pipeline import Sentencizer
+import spacy
+
+nlp = spacy.load("en_core_web_trf")
+# sentencizer = Sentencizer()
+doc = nlp("This is a sentence. This is another sentence.")
+x = [sent for sent in doc.sents]
+print(x)
